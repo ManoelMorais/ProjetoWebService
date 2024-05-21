3efa59147c946cdd8f92d8e57033d92c4b753126
Manoel <999142818m@gmail.com>
2024-05-21 11:15:31 -0300

    H2 database, test progile, JPA

--- src/main/java/com/web/serviceweb/entities/User.java
+++ src/main/java/com/web/serviceweb/entities/User.java
@@ -1,5 +1,6 @@
 package com.web.serviceweb.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -11,12 +12,19 @@ import java.io.Serializable;
 @Setter
 @AllArgsConstructor
 @EqualsAndHashCode
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
+
+    public User() {
+    }
 
 }
