6aa8c6c0c0cb6d0e818607e10b2b24f50c8f872b
Manoel <999142818m@gmail.com>
2024-05-24 10:58:27 -0300

    Category

---
+++ src/main/java/com/web/serviceweb/Service/CategoryService.java
@@ -0,0 +1,25 @@
+package com.web.serviceweb.Service;
+
+import com.web.serviceweb.Repository.CategoryRepository;
+import com.web.serviceweb.entities.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        return obj.get();
+    }
+}
---
+++ src/main/java/com/web/serviceweb/entities/Category.java
@@ -0,0 +1,27 @@
+package com.web.serviceweb.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+}
